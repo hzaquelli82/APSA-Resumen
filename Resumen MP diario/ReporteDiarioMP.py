431b639e1f9f66c16589efa251c2c73c4bc65563
hzaquelli82 <hzaquelli82@gmail.com>
2025-03-22 10:11:09 -0300

    se cambió el saparador por el ';'

--- Resumen MP diario/ReporteDiarioMP.py
+++ Resumen MP diario/ReporteDiarioMP.py
@@ -69,4 +69,4 @@ if not os.path.exists(ruta_carpeta):
     os.mkdir(ruta_carpeta)
     
 # Exportamos el DataFrame a un archivo de Excel
-df_result.to_csv(f"ReporteDiario/{fecha_actual}.csv", index=False, header=False)
+df_result.to_csv(f"ReporteDiario/{fecha_actual}.csv", index=False, header=False, sep=';')
