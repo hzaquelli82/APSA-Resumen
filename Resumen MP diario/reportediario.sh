6fb23989a5e24a3a208526993fe884bfc07ba2ed
hzaquelli82 <hzaquelli82@gmail.com>
2025-06-18 17:00:27 -0300

    Mover carpetas y crear scrip sh

---
+++ Resumen MP diario/reportediario.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#1. Iniciar el entorno virtual
+source /venv/bin/activate
+
+#2. Ejecutar el script de Python
+pyhton ~/APSA-ResumenMP/Resumen\ MP\ diario/ReporteDiarioMP.py
+
+#3. Desactivar el entorno virtual
+deactivate
